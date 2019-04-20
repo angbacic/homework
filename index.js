57f368e80d5e0f850ad351bd7a4b7b070869215d
angbacic <angela.bacic1@gmail.com>
2019-04-20 21:02:56 +0200

    defeated commit

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const songsRouter = require('./songs/routes')
 const authRoutes = require('./auth/routes')
 
 const app = express()
-// const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000
 
 app .use(bodyParser.json())
     .use(playlistRouter)
     .use(songsRouter)
     .use(usersRouter)
     .use(authRoutes)
-    .listen(process.env.PORT || 5000)
-    // .listen(port, ()=> console.log(`Listening on port ${port}`))
+    .listen(port, ()=> console.log(`Listening on port ${port}`))
