dd0d135e7a5e485ba7d36b3bc59e2033976eda9d
angbacic <angela.bacic1@gmail.com>
2019-04-20 11:47:03 +0200

    initial

--- homework/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const playlistRouter = require('./playlists/routes');
-
-const app = express()
-const port = process.env.PORT || 4000
-
-app .use(playlistRouter)
-    .listen(port, ()=> console.log(`Listening on port ${port}`))
